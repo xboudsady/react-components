60749c8929c893a89d8ab5fb42a563564818945b
xboudsady <boudsadyx@gmail.com>
2019-07-30 19:48:45 +0200

    Added props author to CommentDeails

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
         <img src={Faker.image.avatar()} alt="avatar" />
       </a>
       <div className="content">
-        <a href="/" className="author">Sam</a>
+        <a href="/" className="author">{ props.author }</a>
         <div className="metadata"><span className="data">Today at 6:00 pm</span></div>
         <div className="text">Nice blog post!</div>
       </div>
