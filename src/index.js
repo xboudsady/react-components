fd9d186ec7b88fb3d40faeafa79ef5423c0629cb
xboudsady <boudsadyx@gmail.com>
2019-07-30 22:26:23 +0200

    Upadted nested components

--- src/index.js
+++ src/index.js
@@ -8,22 +8,35 @@ const App = () => {
   return (
     <div className="ui container comments">
       <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
         <CommentDetail 
           avatar={Faker.image.avatar()}
           author="Sam" 
           timeAgo="Today at 4:55PM" 
           content="Nice Blog Post!" />
       </ApprovalCard>
-      <CommentDetail 
-        avatar={Faker.image.avatar()}
-        author="Alex" 
-        timeAgo="Today at 2:00PM" 
-        content="I like the subject" />
-      <CommentDetail 
-        avatar={Faker.image.avatar()}
-        author="Jane" 
-        timeAgo="Yesterday at 5:00PM" 
-        content="I like the writing" />
+
+      <ApprovalCard>
+        <CommentDetail 
+          avatar={Faker.image.avatar()}
+          author="Alex" 
+          timeAgo="Today at 2:00PM" 
+          content="I like the subject" />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail 
+          avatar={Faker.image.avatar()}
+          author="Jane" 
+          timeAgo="Yesterday at 5:00PM" 
+          content="I like the writing" />
+      </ApprovalCard>
     </div>
   );
 };
