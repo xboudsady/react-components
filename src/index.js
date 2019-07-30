32fa10793cb5d6f915d7f67b84142a7df01c29f4
xboudsady <boudsadyx@gmail.com>
2019-07-30 02:19:21 +0200

    Adder faker.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">Same</a>
+          <div className="metadata"><span className="data">Today at 6:00 pm</span></div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
